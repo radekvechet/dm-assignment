5f1a2e6ba09a9fa875b83fa2f897fc7e68da700d
Radek Věchet <radek.vechet@gmail.com>
2025-02-02 18:45:00 +0100

    feat: F7 | sort todo items

--- client/src/components/List.tsx
+++ client/src/components/List.tsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import { useTodoItems } from "../services/useTodo"
 import { ListItem } from "./ListItem"
+import { TodoItem } from "../types/todoTypes"
 
 export const ListStyled = styled.div`
   display: flex;
@@ -11,6 +12,16 @@ export const List = () => {
   const { useGetItems } = useTodoItems()
   const { items, loading, error } = useGetItems()
 
+  const sortItems = (a: TodoItem, b: TodoItem): number => {
+    // First, sort by isDone: items with isDone === true should come first.
+    if (a.isDone !== b.isDone) {
+      // If a is done and b is not, a should come first.
+      return (a.isDone ? 1 : 0) - (b.isDone ? 1 : 0)
+    }
+    // If both items isDone param is identical, sort by createdAt (descending).
+    return b.createdAt - a.createdAt
+  }
+
   return (
     <ListStyled>
       {error ? (
@@ -19,15 +30,17 @@ export const List = () => {
         <>
           {loading && <p>Loading...</p>}
           {items &&
-            items.map((item) => (
-              <ListItem
-                itemId={item.id}
-                key={item.id}
-                label={item.label}
-                createdAt={item.createdAt}
-                isDone={item.isDone}
-              />
-            ))}
+            items
+              .sort(sortItems)
+              .map((item) => (
+                <ListItem
+                  itemId={item.id}
+                  key={item.id}
+                  label={item.label}
+                  createdAt={item.createdAt}
+                  isDone={item.isDone}
+                />
+              ))}
         </>
       )}
     </ListStyled>
